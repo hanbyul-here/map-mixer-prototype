e0f24144819d2e6279c96740c32c18af210139ed
Hanbyul Jo <hanbyul@nyu.edu>
2017-10-19 13:08:44 -0400

    touched css slightly

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import CodePanel from './code-panel';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App container">
         <Map />
         <div className="row">
           <Panel />
